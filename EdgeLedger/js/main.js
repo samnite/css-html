f397ee42c6e08c5fc4cde8af59bb0a94e2a7a033
Alexander Gribenchenko <samnite87@gmail.com>
2019-10-23 15:00:47 +0300

    added mobile styles

--- EdgeLedger/js/main.js
+++ EdgeLedger/js/main.js
@@ -11,3 +11,28 @@ function initMap() {
   //Marker
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Sticky menu background
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = '0.8';
+  } else {
+    document.querySelector('#navbar').style.opacity = '1';
+  }
+});
+
+// Smooth Scrolling by JQuery
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
